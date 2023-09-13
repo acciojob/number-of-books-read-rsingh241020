860c557ae7b6ef2e31bce7da2d967916e9480633
Rohit Kumar <126482082+rsingh241020@users.noreply.github.com>
2023-09-13 21:04:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,11 +15,38 @@ const library = [
     readingStatus: false,
   },
 ];
-
+ 
 const numberOfBooksRead = () => {
   // write your code here
+	    // write your code here
+    let count=0;
+      library.forEach((i)=>{
+       if(i.readingStatus===true)
+       {
+         count++;
+       }
+      })
+      return count;
+ 
 };
-
+ 
 // Do not change the code below
-
+ 
+alert(numberOfBooksRead());
+const numberOfBooksRead = () => {
+  // write your code here
+	    // write your code here
+    let count=0;
+      library.forEach((i)=>{
+       if(i.readingStatus===true)
+       {
+         count++;
+       }
+      })
+      return count;
+ 
+};
+ 
+// Do not change the code below
+ 
 alert(numberOfBooksRead());
